58cbec78bbdca2d9dafded793ea145c01c87fb2e
kaller01 <martin@kallers.se>
2020-12-01 13:40:35 +0100

    Key events

--- src/game/GameCore.java
+++ src/game/GameCore.java
@@ -2,18 +2,26 @@ package game;
 
 import javafx.scene.canvas.GraphicsContext;
 
+import java.util.HashMap;
+
 public class GameCore {
     GraphicsContext gc;
     double WIDTH, HEIGHT;
+    private HashMap<String, Boolean> currentlyActiveKeys;
 
-    public GameCore(GraphicsContext gc, double width, double height){
+    public GameCore(GraphicsContext gc, double width, double height) {
         this.gc = gc;
         this.WIDTH = width;
         this.HEIGHT = height;
     }
 
+    public void setCurrentlyActiveKeys(HashMap<String, Boolean> keys) {
+        this.currentlyActiveKeys = keys;
+        System.out.println(keys);
 
-    public void update(double elapsedTime){
+    }
+
+    public void update(double elapsedTime) {
 //        System.out.println(elapsedTime);
 //        System.out.println(1*elapsedTime);
     }
--- src/game/Main.java
+++ src/game/Main.java
@@ -3,22 +3,28 @@ package game;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main extends Application {
-    public static double WIDTH= 1600;
+    public static double WIDTH = 1600;
     public static double HEIGHT = 800;
     public static final int FRAMES_PER_SECOND = 60;
     private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    private HashMap<String, Boolean> currentlyActiveKeys = new HashMap<>();
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         ResizableCanvas canvas = new ResizableCanvas();
         GraphicsContext gc = canvas.getGraphicsContext2D();
         StackPane root = new StackPane();
@@ -35,7 +41,6 @@ public class Main extends Application {
         primaryStage.show();
 
 
-
         System.out.println(canvas.getWidth());
 
         //Game
@@ -50,17 +55,35 @@ public class Main extends Application {
         animation.getKeyFrames().add(frame);
         animation.play();
 
+        //keypress
+        keyListener(primaryStage, game);
 
     }
 
-    public void resizeListener(Canvas canvas){
+    public void keyListener(Stage primaryStage, GameCore game) {
+        primaryStage.getScene().setOnKeyPressed(event -> {
+            String codeString = event.getCode().toString();
+            if (!currentlyActiveKeys.containsKey(codeString)) {
+                currentlyActiveKeys.put(codeString, true);
+                game.setCurrentlyActiveKeys(currentlyActiveKeys);
+            }
+        });
+        primaryStage.getScene().setOnKeyReleased(event -> {
+            currentlyActiveKeys.remove(event.getCode().toString());
+            game.setCurrentlyActiveKeys(currentlyActiveKeys);
+        });
+
+
+    }
+
+    public void resizeListener(Canvas canvas) {
         canvas.widthProperty().addListener((obs, oldVal, newVal) -> {
             WIDTH = (double) newVal;
-            System.out.println("Width: "+WIDTH + " Height: "+HEIGHT);
+            System.out.println("Width: " + WIDTH + " Height: " + HEIGHT);
         });
         canvas.heightProperty().addListener((obs, oldVal, newVal) -> {
             HEIGHT = (double) newVal;
-            System.out.println("Width: "+WIDTH + " Height: "+HEIGHT);
+            System.out.println("Width: " + WIDTH + " Height: " + HEIGHT);
         });
         WIDTH = canvas.getWidth();
         HEIGHT = canvas.getHeight();
