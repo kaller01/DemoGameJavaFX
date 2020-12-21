2104941e98beb04d3d9b2444cdc4fead0df2fb16
kaller01 <martin@kallers.se>
2020-12-21 12:56:44 +0100

    Onrezize fix

--- src/game/Guest.java
+++ src/game/Guest.java
@@ -4,14 +4,22 @@ import javafx.scene.canvas.GraphicsContext;
 import server.Client;
 import server.Multiplayer;
 
+import java.util.HashMap;
+
 public class Guest extends GameCore {
     Multiplayer guest;
 
     public Guest(GraphicsContext gc, double width, double height, Multiplayer guest) {
         super(gc, width, height);
         this.guest = guest;
-        guest.start();
         onResize();
+    }
+
+    @Override
+    void onResize() {
+        if(entityManager != null){
+            super.onResize();
+        }
     }
 
     @Override
@@ -20,7 +28,6 @@ public class Guest extends GameCore {
 
         if(guest.isConnected()){
             guest.getConnection().setUpdate(currentlyActiveKeys);
-            guest.getConnection().update();
             entityManager = (EntityManager) guest.getConnection().read();
             if(entityManager != null){
                 entityManager.drawAll();
